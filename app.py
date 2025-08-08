ec714a70d4f4df53efd41633fbf860bc5f82a562
mrfrankelizondo <85322328+mrfrankelizondo@users.noreply.github.com>
2025-08-07 22:04:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 
-from risk_range import fetch_prices, compute_indicators, build_risk_range, make_table
+from risk_range import fetch_prices, compute_indicators, build_risk_range, mak
+
 
 st.set_page_config(page_title="Multi-Stock Risk Range", layout="wide")
 
